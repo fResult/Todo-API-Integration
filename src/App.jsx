6ff93853db55c775f54ccecdc4872e16d4d8d2b2
fResult <styxmaz@gmail.com>
2022-10-08 20:53:34 +0700

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      HELLO TODO
+    </>
+  )
+}
+
+export default App
